6cdde3f8af9e44d8b2c45824082a2964bce4766b
Jacob Hensley <jacobrhensley3@gmail.com>
2021-01-25 17:01:02 -0600

    answer array.diff

--- codeWars.js
+++ codeWars.js
@@ -13,4 +13,11 @@ function truncateString(str, num) {
     return str;
   }
 
-  
+//   Your goal in this kata is to implement a difference function, which subtracts one list from another and returns the result.
+
+// It should remove all values from list a, which are present in list b.
+
+function arrayDiff(a, b) {
+    let difference = a.filter(x => !b.includes(x));
+    return difference
+  }
